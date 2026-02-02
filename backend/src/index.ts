92f70e320fa38e97744294fce95d7a9c6bd94c0c
Thomas Rabillon <thomas.rabillon@alumni.univ-avignon.fr>
2026-02-02 17:06:58 +0100

    partie 1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,20 @@ import router from "./interface/routes/route";
 const https = require('https');
 const fs = require('fs');
 
+var session = require('express-session')
+
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+	secret: 'test',
+	saveUninitialized: false,
+	resave: false,
+	cookie : {maxAge : 24 * 3600 * 1000}
+}))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 app.use('/api', router);
 
 const options = {
@@ -15,9 +26,9 @@ const options = {
 	cert: fs.readFileSync('./ssl/cert.pem')
 };
 
-app.listen(config.port.HTTP, () => {
-	console.log('Server is running on port ' + config.port.HTTP);
-});
+// app.listen(config.port.HTTP, () => {
+// 	console.log('Server is running on port ' + config.port.HTTP);
+// });
 
 https.createServer(options, app).listen(config.port.HTTPS, () => {
 	console.log('HTTPS => listening on ' + config.port.HTTPS);
--- backend/src/interface/routes/route.ts
+++ backend/src/interface/routes/route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import authRoute from "./auth/route";
 
 const router = Router();
 
+router.use('/auth', authRoute);
+
 router.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
 export default router;
