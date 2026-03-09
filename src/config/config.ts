980fa329ef100a840047e8d860ccf417f1b7c89e
Thomas_Rabillon <thomas.rabillon@alumni.univ-avignon.fr>
2026-03-09 10:15:39 +0100

    test connection postgres

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,10 +4,17 @@ dotenv.config();
 
 const config = {
   port: {
-    HTTP : Number(process.env.PORT) || 3116,
-    HTTPS : Number(process.env.PORT) || 3117
+    HTTP : Number(process.env.HTTP_PORT) || 3116,
+    HTTPS : Number(process.env.HTTPS_PORT) || 3117
   },
   nodeEnv: process.env.NODE_ENV || 'development',
+  postgres: {
+    HOST : process.env.DB_HOST,
+    USER : process.env.DB_USER,
+    MDP : process.env.DB_MDP,
+    PORT : Number(process.env.DB_PORT),
+    DB : process.env.DB_NAME
+  }
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
 import express from "express";
 import config from "./config/config";
 
+import crypto, { createHash, createHmac } from "crypto";
+
+import { Connection, Pool } from "pg";
+
+const pool = new Pool({
+    user : config.postgres.USER,
+    host : config.postgres.HOST,
+    database : config.postgres.DB,
+    password : config.postgres.MDP,
+    port : config.postgres.PORT
+});
+
+const test_mdp = createHash('sha1').update('test').digest('hex');
+
+pool.connect((err, client, done) => {
+    if(err) { console.log('Error connecting to pg server' + err.stack); } 
+    else{
+        console.log('Connection established / pg db server');
+        
+        client?.query("SELECT * FROM fredouil.compte WHERE id = 7;", (err, result) => {
+            if(err){
+                console.log('Erreur d’exécution de la requete' + err.stack);
+            } else if(result.rows[0] != null && result.rows[0].motpasse == test_mdp) {
+                //request.session.isConnected = true; // utilisation des sessions
+                console.log(result.rows[0].prenom);
+            } else {
+                console.log('Connexion échouée : informations de connexion incorrecte');
+            }
+        });
+    }
+});
+
+
+
 const https = require('https');
 const fs = require('fs');
 
